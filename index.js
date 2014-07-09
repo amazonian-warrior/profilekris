8c068f4449e38d61779544cd5ad6ba3b17292569
kymolina <molina0328@gmail.com>
2014-07-09 19:22:33 -0400

    created two drop down menus and a button that adds them together

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 function clicker() {
-	alert("Yay, I controlled you!");
+	alert("Hello all!");
 };
+
+
+
+function add() {
+	var num1 = parseInt(document.getElementById("list1").value);
+	console.log("num1: " + num1);
+	var num2 = parseInt(document.getElementById("list2").value);
+	console.log("num2: " + num2);
+	var answer = num1 + num2;
+	console.log("answer: " + answer);
+	alert(answer);
+}
 
