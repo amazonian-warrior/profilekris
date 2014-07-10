f9afe2d3a2328a6eb55544dfba130cdbb82184d6
kymolina <molina0328@gmail.com>
2014-07-09 20:21:05 -0400

    added a multiply, divide, and exponent buttons

--- index.js
+++ index.js
@@ -6,11 +6,47 @@ function clicker() {
 
 function add() {
 	var num1 = parseInt(document.getElementById("list1").value);
-	console.log("num1: " + num1);
 	var num2 = parseInt(document.getElementById("list2").value);
-	console.log("num2: " + num2);
 	var answer = num1 + num2;
-	console.log("answer: " + answer);
 	alert(answer);
 }
 
+function multiply (){
+	var x = parseInt(document.getElementById("list1").value);
+	var y = parseInt(document.getElementById("list2").value);
+	var answer = x * y;
+	alert(answer);
+}
+
+function divide () {
+	var x = parseInt(document.getElementById("list1").value);
+	var y = parseInt(document.getElementById("list2").value);
+	var answer = x / y;
+	alert(answer);
+
+}
+
+function exponential (){
+	var x = parseInt(document.getElementById("list1").value);
+	var y = parseInt(document.getElementById("list2").value);
+	var answer = 1;
+	for (var i = 0; i < y; i++) {
+		answer = answer * x;
+	}
+	alert(answer);
+
+}
+
+function forLoopExample() {
+	var i = 0;
+	for(i=0; i<5; i++) {
+		console.log('hi');
+	}
+}
+// alert(answer);
+
+	// var answer = Math.pow(x, y);
+
+// 2 2 2 
+
+// 2 to the 3 
